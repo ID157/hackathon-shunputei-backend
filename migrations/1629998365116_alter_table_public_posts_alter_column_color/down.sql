9be5a9045cde14e732a44858a7e043e50bd54008
id157 <id2000@outlook.jp>
2021-08-27 12:50:15 +0900

    change col name

---
+++ migrations/1629998365116_alter_table_public_posts_alter_column_color/down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."posts" ALTER COLUMN "color" TYPE integer;
+alter table "public"."posts" rename column "type" to "color";
---
+++ migrations/1629998365116_alter_table_public_posts_alter_column_color/up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "public"."posts" ALTER COLUMN "color" TYPE int4;
+alter table "public"."posts" rename column "color" to "type";
